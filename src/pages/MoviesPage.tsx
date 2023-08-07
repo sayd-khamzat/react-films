47fe9e3641480a6acdc61476421537719b52a1b9
S-KH <tadaev050@gmail.com>
2023-08-08 01:21:54 +0300

    Added preloader

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -3,12 +3,13 @@ import {useLazyGetMoviesQuery} from '../store/kinopoisk/kinopoisk.api';
 import {useActions} from '../hooks/actions';
 import {useNavigate} from 'react-router-dom';
 import {Paginator} from '../components/common/Paginator';
+import {Preloader} from '../components/common/Preloader';
 
 export function MoviesPage() {
 
     const [currentPage, setCurrentPage] = useState(1)
 
-    const [getMovies, {data: moviesData, isLoading, isError}] = useLazyGetMoviesQuery()
+    const [getMovies, {data: moviesData, isFetching, isError}] = useLazyGetMoviesQuery()
     const {addMovieId} = useActions()
     const navigate = useNavigate()
 
@@ -30,29 +31,32 @@ export function MoviesPage() {
             <div className='mx-auto pt-5 w-[90%] text-slate-800'>
                 <h1 className='text-center text-3xl font-semibold'>Фильмы</h1>
 
-                {isLoading && <p className='text-center mt-5 text-lg font-semibold'>Загрузка фильмов...</p>}
-                {isError &&
-                    <p className='text-red-600 text-center mt-5 text-lg font-semibold'>Произошла какая-то ошибка...</p>}
-
-                <div className='grid grid-cols-5 mt-14 justify-items-center'>
-                    {moviesData?.docs.map(movie =>
-                        <div key={movie.id}>
-                            <div className='bg-slate-300 w-[200px] h-[500px] text-sm rounded-lg shadow-md mb-10 hover:text-indigo-600 transition-colors cursor-pointer'>
-                                <img src={movie.poster.url}
-                                     onClick={() => clickHandler(movie.id)}
-                                     className='mt-3 rounded'
-                                     alt="logo">
-                                </img>
-                                <div className='ml-1 bg-slate-300'>
-                                    <h1 className='text-lg font-semibold mb-2'>{movie.name}</h1>
-                                    <p>Жанры: {movie.genres.map((g, i) => <span key={i}>{g.name} </span>)}</p>
-                                    <p>{movie.year} <span>год</span></p>
-                                    <p>Рейтинг: {movie.rating.kp}</p>
+                {isFetching || isError
+                    ? isFetching
+                        ? <Preloader/>
+                        : <p className='text-red-600 text-center mt-5 text-lg font-semibold'>Произошла какая-то
+                            ошибка...</p>
+                    : <div className='grid grid-cols-5 mt-14 justify-items-center'>
+                        {moviesData?.docs.map(movie =>
+                            <div key={movie.id}>
+                                <div
+                                    className='bg-slate-300 w-[200px] h-[500px] text-sm rounded-lg shadow-md mb-10 hover:text-indigo-600 transition-colors cursor-pointer'>
+                                    <img src={movie.poster.url}
+                                         onClick={() => clickHandler(movie.id)}
+                                         className='mt-3 rounded'
+                                         alt="logo">
+                                    </img>
+                                    <div className='ml-1 bg-slate-300'>
+                                        <h1 className='text-lg font-semibold mb-2'>{movie.name}</h1>
+                                        <p>Жанры: {movie.genres.map((g, i) => <span key={i}>{g.name} </span>)}</p>
+                                        <p>{movie.year} <span>год</span></p>
+                                        <p>Рейтинг: {movie.rating.kp}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )}
-                </div>
+                        )}
+                    </div>
+                }
             </div>
             <Paginator currentPage={currentPage}
                        setCurrentPage={setCurrentPage}
